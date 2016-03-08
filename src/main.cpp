33413a070ca5ac2f60aaadadde8a5a34caeddfdc
Frawless <xstejs24@stud.fit.vutbr.cz>
2016-03-08 12:58:33 +0100

    Initial commit-2

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: frawless
+ *
+ * Created on 8. březen 2016, 12:51
+ */
+
+#include <cstdlib>
+#include <sys/types.h>
+#include <unistd.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+	std::cerr<<"Začínáme s Othellem"<<std::endl;
+	return 0;
+}
+
